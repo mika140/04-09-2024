33f552e86b7b8fdbb34c76f5594fffdc364296b0
BENICHAI MIKHAEL 140 <benichaimikhael@gmail.com>
2024-09-05 02:04:55 +0300

    Add files via upload

---
+++ hw 2 verssion 1.py
@@ -0,0 +1,14 @@
+#start
+
+test:int = int (input('result'));
+if test>= 0 and test <=40:
+    print ('try harder next time')
+elif test>= 41 and test<=60:
+    print ('youre getting there, need some more work')
+elif test>= 61 and test<=80:
+    print('pretty good')
+elif test>= 81 and test<=95:
+    print('awesorme!')
+elif test>= 96 and test<=100:
+    print ('illegal grade')
+# end
