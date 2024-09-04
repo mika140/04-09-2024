ce3ad0be624e001fe090ff75ece1abcf5ca7722a
BENICHAI MIKHAEL 140 <benichaimikhael@gmail.com>
2024-09-05 01:23:26 +0300

    Add files via upload

--- hw 1 b.py
+++ hw 1 b.py
@@ -1,7 +1,9 @@
 #start
 from colorsys import yiq_to_rgb
 
-name1:str =(input('name1'));
-name2:str =(input('name2'));
-name3:str=(input('name3'));
-print (f'{name1:}\n{name2:}\n{name3:}')
+name1:str ='avi'
+name2:str ='yosi'
+name3:str='liron'
+print (f'name:{name1}')
+print (f'name:{name2}')
+print (f'name:{name3}')
