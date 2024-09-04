11c92e4e6f81d7f94b3ea17ea9a8a5a63fccf4fa
BENICHAI MIKHAEL 140 <benichaimikhael@gmail.com>
2024-09-05 00:35:44 +0300

    Add files via upload

---
+++ hw 1.py
@@ -0,0 +1,12 @@
+# start
+
+
+last_name  : str = (input(' id  last name'));
+
+first_name: str= (input(' name'));
+
+height: float=(input('height'));
+
+year:int =(input('year'));
+
+print (f'{last_name } {first_name } { height} {year}')
